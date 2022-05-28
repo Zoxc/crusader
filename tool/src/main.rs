c7ad2697db397c612ca45e498609e1f31af735b5
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2022-05-28 16:14:08 +0200

    init

---
+++ tool/src/main.rs
@@ -0,0 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    Add { name: Option<String> },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Add { name } => {
+            println!("'myapp add' was used, name is: {:?}", name)
+        }
+    }
+}
