cafc14a661e64440b29859aa638a6873b36709e3
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2024-08-04 18:35:26 +0200

    Try to use system fonts

--- src/crusader-gui/src/main.rs
+++ src/crusader-gui/src/main.rs
@@ -5,11 +5,18 @@
 )]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use std::sync::Arc;
+use std::{error::Error, sync::Arc};
 
 use crusader_gui_lib::Tester;
 use crusader_lib::LIB_VERSION;
-use eframe::{egui, emath::vec2, Theme};
+use eframe::{
+    egui::{self, Context, FontData, FontDefinitions, FontFamily},
+    emath::vec2,
+    Theme,
+};
+use font_kit::{
+    family_name::FamilyName, handle::Handle, properties::Properties, source::SystemSource,
+};
 
 fn main() {
     let mut options = eframe::NativeOptions::default();
@@ -28,12 +35,82 @@ fn main() {
     eframe::run_native(
         &format!("Crusader Network Tester {}", LIB_VERSION),
         options,
-        Box::new(move |_cc| {
+        Box::new(move |cc| {
+            let ctx = &cc.egui_ctx;
+            let mut style = ctx.style();
+            let style_ = Arc::make_mut(&mut style);
+            style_.spacing.button_padding = vec2(6.0, 0.0);
+            style_.spacing.interact_size.y = 30.0;
+            style_.spacing.item_spacing = vec2(5.0, 5.0);
+
+            let font_size = 18.0;
+
+            style_.text_styles.get_mut(&egui::TextStyle::Body).map(|v| {
+                v.size = font_size;
+            });
+            style_
+                .text_styles
+                .get_mut(&egui::TextStyle::Button)
+                .map(|v| {
+                    v.size = font_size;
+                });
+            style_
+                .text_styles
+                .get_mut(&egui::TextStyle::Small)
+                .map(|v| {
+                    v.size = font_size;
+                });
+            style_
+                .text_styles
+                .get_mut(&egui::TextStyle::Monospace)
+                .map(|v| {
+                    v.size = font_size;
+                });
+            ctx.set_style(style);
+
+            load_system_font(ctx).ok();
+
             Box::new(App {
                 tester: Tester::new(settings),
             })
         }),
     );
+}
+
+fn load_system_font(ctx: &Context) -> Result<(), Box<dyn Error>> {
+    let mut fonts = FontDefinitions::default();
+
+    let handle = SystemSource::new().select_best_match(
+        &[
+            FamilyName::Title("system-ui".to_string()),
+            FamilyName::Title("Segoe UI".to_string()),
+            FamilyName::SansSerif,
+        ],
+        &Properties::new(),
+    )?;
+
+    let buf: Vec<u8> = match handle {
+        Handle::Memory { bytes, .. } => bytes.to_vec(),
+        Handle::Path { path, .. } => std::fs::read(path)?,
+    };
+
+    const UI_FONT: &str = "System Sans Serif";
+
+    fonts
+        .font_data
+        .insert(UI_FONT.to_owned(), FontData::from_owned(buf));
+
+    if let Some(vec) = fonts.families.get_mut(&FontFamily::Proportional) {
+        vec.insert(0, UI_FONT.to_owned());
+    }
+
+    if let Some(vec) = fonts.families.get_mut(&FontFamily::Monospace) {
+        vec.insert(0, UI_FONT.to_owned());
+    }
+
+    ctx.set_fonts(fonts);
+
+    Ok(())
 }
 
 struct App {
@@ -42,13 +119,6 @@ struct App {
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let mut style = ctx.style();
-        let style_ = Arc::make_mut(&mut style);
-        style_.spacing.button_padding = vec2(6.0, 0.0);
-        style_.spacing.interact_size.y = 30.0;
-        style_.spacing.item_spacing = vec2(5.0, 5.0);
-        ctx.set_style(style);
-
         egui::CentralPanel::default().show(ctx, |ui| {
             self.tester.show(ctx, ui);
         });
